87bcfadce7a780b94fd4b8ceb7c566b3d9628b22
Jorge Canales <JLP.canales21@gmail.com>
2020-10-13 02:26:24 -0500

    Removing commented code

--- src/main/java/com/example/GeorgeItexico/rest/api/ReservationsController.java
+++ src/main/java/com/example/GeorgeItexico/rest/api/ReservationsController.java
@@ -22,7 +22,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping(path = "/george")
 @Tag(name = "Reservations")
 public class ReservationsController {
 
